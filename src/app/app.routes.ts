14311833c2accc2c746deda802cf59d19db66b68
Tobin Zühlke <zuhlketobin@gmail.com>
2026-01-11 22:57:52 +0100

    Added placeholder home component, removed placeholder from app component, added login component and home component as a target for valid routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { HomeComponent } from './home/components/home-component/home-component';
+import { LoginComponent } from './login/components/login.component/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [AuthGuard] // protected route
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login' // fallback
+  }
+];
