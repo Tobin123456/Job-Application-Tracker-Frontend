062af21f30218006afa3722f5938f230bc77bce9
Tobin Zühlke <zuhlketobin@gmail.com>
2026-01-12 04:01:23 +0100

    Removed unneded provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ConfigService } from './shared/services/config/config.service';
+import { authInterceptor } from './shared/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
-    ConfigService, // make it injectable
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAppInitializer(() => {
       const configService = inject(ConfigService);
       return configService.load(); // async load JSON and build URLs
